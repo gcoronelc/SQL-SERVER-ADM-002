7a8198ae0011522f9a37e624899bb7536935ef70
Gustavo Coronel <gcoronelc@gmail.com>
2020-03-05 20:20:37 -0500

    Clase 08

---
+++ Clase08/SSIS/EDUTECT-CONSULTA.sql
@@ -0,0 +1,41 @@
+-- CONSULTA
+
+use edutec;
+go
+
+
+select * from edutec.dbo.CursoProgramado;
+go
+
+CREATE VIEW DBO.V_RESUMEN_VENTAS
+AS
+With
+v1 as (
+	select 
+		idciclo, IdCurso,
+		count(1) Secciones, 
+		sum(Matriculados) Matriculados,
+		sum(Matriculados * PreCursoProg) Ingresos
+	from edutec.dbo.CursoProgramado
+	where Activo = 1
+	group by idciclo, IdCurso
+)
+select 
+	v1.IdCiclo, v1.IdCurso, c.NomCurso, 
+	v1.Secciones, v1.Matriculados, v1.Ingresos
+from EduTec.dbo.Curso c
+join v1 on c.IdCurso = v1.IdCurso;
+GO
+
+-- CREACION DE LA TABLA
+SELECT * INTO DBO.TB_RESUMEN_VENTAS
+FROM DBO.V_RESUMEN_VENTAS
+WHERE IdCiclo = '12345';
+GO
+
+-- LIMPIA LA TABLA
+TRUNCATE TABLE DBO.TB_RESUMEN_VENTAS;
+GO
+
+
+
